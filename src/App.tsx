5600da3c03b609d54c7810f5a1a8eb98415cc3f7
hiro <hirohanahaba@gmail.com>
2022-06-07 05:09:15 +0900

    GitHubを利用したフロント側による外部認証実装

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [message, setMessage] = useState("");
-  useEffect(() => {
-    fetch("https://hirohana-backend.herokuapp.com/api")
+
+  const onAuthGitHubHandler = () => {
+    fetch("http://localhost:5000/api")
       .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  };
   return (
     <div className="App">
       <h1>フロントエンド</h1>
+      <button onClick={() => onAuthGitHubHandler()}>GitHubログイン認証</button>
       <p>{message}</p>
     </div>
   );
