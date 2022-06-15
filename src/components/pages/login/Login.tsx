04c9ca3cbf11e45abfc191bc92c52a7a605ff908
hiro <hirohanahaba@gmail.com>
2022-06-15 22:42:34 +0900

    途中家庭

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import AuthGithub from "components/molecules/authGithub/AuthGithub";
 import DefaultLayout from "components/templates/defaultLayout/DefaultLayout";
 import styles from "./Login.module.scss";
+import AuthAccount from "components/molecules/authAccount/AuthAccount";
 
 const Login = () => {
   return (
     <DefaultLayout>
       <div>Login</div>
+      <AuthAccount />
       <AuthGithub />
     </DefaultLayout>
   );
