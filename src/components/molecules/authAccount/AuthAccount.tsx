0be45417b5807c4ca37c646c1eb173a83eb32c55
hiro <hirohanahaba@gmail.com>
2022-06-16 17:58:13 +0900

    途中経過

--- src/components/molecules/authAccount/AuthAccount.tsx
+++ src/components/molecules/authAccount/AuthAccount.tsx
@@ -1,15 +1,19 @@
 import React, { useRef } from "react";
-import axios from "axios";
+import { useDispatch } from "react-redux";
+
+import { login } from "reducks/user/actionCreator";
+import sweetAlertOfSuccess from "utils/sweetAlert/sweetAlertofSuccess";
 
 const AuthAccount = () => {
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+  const dispatch = useDispatch();
 
-  const authAccountHandler = async (e) => {
+  const authAccountHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const payload = {
-      email: emailRef.current.value,
-      password: passwordRef.current.value,
+      email: emailRef.current!.value,
+      password: passwordRef.current!.value,
     };
     const response = await fetch("http://localhost:5000/account/login", {
       method: "POST",
@@ -18,10 +22,17 @@ const AuthAccount = () => {
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
-        "Access-Control-Allow-Credentials": true,
       },
     });
-    const data = await response.json();
+    const { message, user } = await response.json();
+    dispatch(
+      login({
+        uid: user[0].id,
+        displayName: user[0].displayName,
+        photoUrl: user[0].photoUrl,
+      })
+    );
+    sweetAlertOfSuccess(message);
   };
 
   return (
