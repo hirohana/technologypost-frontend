803eaaf519d471abd5a285d59b58eca4fcbe9e3e
hiro <hirohanahaba@gmail.com>
2022-06-22 08:08:11 +0900

    articles周辺のバックエンドからフロント側へのデータ取り込み完了

--- src/types/articles/articles.ts
+++ src/types/articles/articles.ts
@@ -13,17 +13,23 @@ export type ARTICLES_DATA = {
 }[];
 
 export type ARTICLE_DATA = {
-  username: string;
-  user_photo_url: string;
-  user_id: number;
-  title: string;
-  letter_body: string;
-  created_at: string;
-  article_id: number;
-  article_photo_url: null | string;
-  category_name: null | string;
-  comment: null | string;
-  comment_created_at: null | string;
+  data: {
+    article_id: number;
+    article_photo_url: null | string;
+    category_name: null | string;
+    created_at: string;
+    letter_body: string;
+    title: string;
+    user_id: number;
+    user_photo_url: string;
+    username: string;
+  }[];
+  comments: {
+    user_comment: null | string;
+    comment_created_at: null | string;
+    username: string;
+    user_photo_url: string;
+  }[];
 };
 
 export type ARTICLES_DATA_AND_PAGINATION = {
