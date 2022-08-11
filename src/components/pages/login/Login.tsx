af1f0bd1b0fe290da3b202731346e75007d8418c
hiro <hirohanahaba@gmail.com>
2022-08-11 10:25:52 +0900

    jwtトークンをバックエンド側で使用してログイン管理を行うことによる、login.tsx周りのカスタムフック修正

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any*/
 
-import { useState } from 'react';
+import { useState } from "react";
 
 import {
   Avatar,
@@ -9,14 +9,14 @@ import {
   Typography,
   Box,
   TextField,
-} from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-import styles from './Login.module.scss';
-import { useAuthLoginAndSignUp } from 'hooks/components/Login/useAuthLoginAndSignUp';
-import DefaultLayout from 'components/templates/defaultLayout/DefaultLayout';
-import { useIsLoading } from 'hooks/redux/isLoading/useIsLoading';
-import { LoadingIcon } from 'components/atoms/loadingIcon/LoadingIcon';
+import styles from "./Login.module.scss";
+import { useAuthLoginAndSignUp } from "hooks/components/Login/useAuthLoginAndSignUp";
+import DefaultLayout from "components/templates/defaultLayout/DefaultLayout";
+import { useIsLoading } from "hooks/redux/isLoading/useIsLoading";
+import { LoadingIcon } from "components/atoms/loadingIcon/LoadingIcon";
 
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -34,7 +34,6 @@ const Login = () => {
     authLogin,
     signUp,
   } = useAuthLoginAndSignUp();
-  const { isLoading } = useIsLoading();
 
   const isLoginTrueClass = () => {
     return !email || !password
@@ -48,138 +47,128 @@ const Login = () => {
       : styles.submit_button;
   };
 
-  if (!isLoading) {
-    return (
-      <DefaultLayout>
-        <div className={styles.global_container}>
-          <div className={styles.container}>
-            <div className={styles.form_container}>
-              <Avatar
-                className={
-                  isLogin ? styles.avatar__login : styles.avatar__signup
-                }
-              ></Avatar>
-              <Typography
-                component="h1"
-                variant="h5"
-                className={styles.typography}
-              >
-                {isLogin ? 'ログイン' : 'アカウント登録'}
-              </Typography>
-              <form onSubmit={(e) => (isLogin ? authLogin(e) : signUp(e))}>
-                {!isLogin && (
-                  <>
-                    <Box textAlign="right">
-                      <label>
-                        <AccountCircleIcon
-                          fontSize="large"
-                          className={
-                            image
-                              ? styles.login_addIconLoaded
-                              : styles.login_addIcon
-                          }
-                        />
-                        <input
-                          className={styles.login_hiddenIcon}
-                          type="file"
-                          onChange={changeImageHandler}
-                          required
-                          disabled={image ? true : false}
-                        />
-                      </label>
-                    </Box>
-                    <TextField
-                      id="username"
-                      label="ユーザー名"
-                      name="username"
-                      variant="outlined"
-                      fullWidth
-                      required
-                      className={styles.textfield}
-                      value={userName}
-                      onChange={(e) => setUserName(e.target.value)}
-                    />
-                  </>
-                )}
-                <TextField
-                  id="email"
-                  label="Eメールアドレス"
-                  name="email"
-                  variant="outlined"
-                  fullWidth
-                  required
-                  className={styles.textfield}
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <TextField
-                  name="password"
-                  label="パスワード"
-                  type="password"
-                  id="password"
-                  required
-                  fullWidth
-                  variant="outlined"
-                  value={password}
-                  className={styles.textfield}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                {isLogin || (
+  return (
+    <DefaultLayout>
+      <div className={styles.global_container}>
+        <div className={styles.container}>
+          <div className={styles.form_container}>
+            <Avatar
+              className={isLogin ? styles.avatar__login : styles.avatar__signup}
+            ></Avatar>
+            <Typography
+              component="h1"
+              variant="h5"
+              className={styles.typography}
+            >
+              {isLogin ? "ログイン" : "アカウント登録"}
+            </Typography>
+            <form onSubmit={(e) => (isLogin ? authLogin(e) : signUp(e))}>
+              {!isLogin && (
+                <>
+                  <Box textAlign="right">
+                    <label>
+                      <AccountCircleIcon
+                        fontSize="large"
+                        className={
+                          image
+                            ? styles.login_addIconLoaded
+                            : styles.login_addIcon
+                        }
+                      />
+                      <input
+                        className={styles.login_hiddenIcon}
+                        type="file"
+                        onChange={changeImageHandler}
+                        required
+                        disabled={image ? true : false}
+                      />
+                    </label>
+                  </Box>
                   <TextField
-                    name="retypingPassword"
-                    label="確認用パスワード"
-                    type="password"
+                    id="username"
+                    label="ユーザー名"
+                    name="username"
                     variant="outlined"
                     fullWidth
-                    id="retypingPassword"
                     required
-                    value={retypingPassword}
                     className={styles.textfield}
-                    onChange={(e) => setRetypingPassword(e.target.value)}
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
                   />
-                )}
-
-                <Button
+                </>
+              )}
+              <TextField
+                id="email"
+                label="Eメールアドレス"
+                name="email"
+                variant="outlined"
+                fullWidth
+                required
+                className={styles.textfield}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                name="password"
+                label="パスワード"
+                type="password"
+                id="password"
+                required
+                fullWidth
+                variant="outlined"
+                value={password}
+                className={styles.textfield}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {isLogin || (
+                <TextField
+                  name="retypingPassword"
+                  label="確認用パスワード"
+                  type="password"
+                  variant="outlined"
                   fullWidth
-                  variant="contained"
-                  className={isLogin ? isLoginTrueClass() : isLoginFalseClass()}
-                  type="submit"
-                  disabled={
-                    isLogin
-                      ? !email || !password
-                      : !userName ||
-                        !email ||
-                        !password ||
-                        !retypingPassword ||
-                        !image
-                  }
-                >
-                  {isLogin ? 'ログイン' : 'アカウント登録'}
-                </Button>
-                <Grid container>
-                  <Grid item>
-                    <span
-                      className={styles.login_toggleMode}
-                      onClick={() => setIsLogin(!isLogin)}
-                    >
-                      {isLogin ? 'アカウント登録画面へ' : 'ログイン認証画面へ'}
-                    </span>
-                  </Grid>
+                  id="retypingPassword"
+                  required
+                  value={retypingPassword}
+                  className={styles.textfield}
+                  onChange={(e) => setRetypingPassword(e.target.value)}
+                />
+              )}
+
+              <Button
+                fullWidth
+                variant="contained"
+                className={isLogin ? isLoginTrueClass() : isLoginFalseClass()}
+                type="submit"
+                disabled={
+                  isLogin
+                    ? !email || !password
+                    : !userName ||
+                      !email ||
+                      !password ||
+                      !retypingPassword ||
+                      !image
+                }
+              >
+                {isLogin ? "ログイン" : "アカウント登録"}
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <span
+                    className={styles.login_toggleMode}
+                    onClick={() => setIsLogin(!isLogin)}
+                  >
+                    {isLogin ? "アカウント登録画面へ" : "ログイン認証画面へ"}
+                  </span>
                 </Grid>
-              </form>
-            </div>
+              </Grid>
+            </form>
           </div>
-          <div className={styles.background_image}></div>
         </div>
-      </DefaultLayout>
-    );
-  } else {
-    return (
-      <DefaultLayout>
-        <LoadingIcon />
-      </DefaultLayout>
-    );
-  }
+        <div className={styles.background_image}></div>
+      </div>
+    </DefaultLayout>
+  );
 };
 
 export default Login;
